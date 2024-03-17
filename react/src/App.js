d1e0d49c631c536e3237072cebcd2b40ba9b04f9
troombox <45360723+troombox@users.noreply.github.com>
2024-03-17 20:53:51 +0000

    Add files via upload

---
+++ react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Login from './Login';
+// import './App.css'; // Assuming your CSS rules are here or in individual CSS modules
+import './tailwind.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
